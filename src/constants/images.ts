c4ce1c7c1a7e7e9d2ae0df41866d0877a19ba47b
dev01 <victorcorassa@gmail.com>
2023-02-02 22:58:00 -0300

    feat: app images

---
+++ src/constants/images.ts
@@ -0,0 +1,8 @@
+export const Images = {
+  wind: require('../assets/img/wind.png'),
+  sun: require('../assets/img/sun.png'),
+  leaf: require('../assets/img/leaf.png'),
+  graphicRed: require('../assets/img/graphic-red.png'),
+  graphicGreen: require('../assets/img/graphic-green.png'),
+  businessStatistics: require('../assets/img/business-statistics.png'),
+};
